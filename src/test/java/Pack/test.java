eb3c18544df17f94a2b3f4c55f7b3204039fb694
Nandhini-1999 <nandhinivelmurugan27111999@gmail.com>
2022-03-14 10:01:51 +0530

    sending to project to git

---
+++ src/test/java/Pack/test.java
@@ -0,0 +1,26 @@
+package Pack;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test {
+
+	@Test
+	public void tester() {
+		Addition a=new Addition();
+		int actual=a.add(20, 40);
+		int expected=60;
+		assertEquals(expected,actual);
+		
+	}
+	@Test
+	public void tester1() {
+		Addition a=new Addition();
+		int actual=a.add(25, 40);
+		int expected=65;
+		assertEquals(expected,actual);
+		
+	}
+
+}
